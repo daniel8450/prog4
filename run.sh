507c93e5e007512e672b5d93446fb8f9d86816da
Daniel LaBorde <dlabo14@lsu.edu>
2017-04-16 17:20:36 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 echo
-echo "///BABELFISH/// I partially got the Java translation working, but no other languages are supported at this time.///"
+echo "///BABELFISH I partially got the Java translation working, but no other languages are supported at this time.///"
 ./babelfish.class
 echo
 echo
 echo
-echo "// I keep getting  a run permission denied error and I cannot figure out the problem//"
-echo "//This is what the code prints when I run it with the sum.alg file//"
+echo "I keep getting  a run permission denied error and I cannot figure out the problem"
+echo "This is what the code prints when I run it with the sum.alg file"
 echo
-echo
+echo "////////////////////////////////////////////////////"
 echo "public static int sum(int n)"
 echo "{"
 echo ";"
@@ -23,7 +23,7 @@ echo ";"
 echo "sum(10)"
 echo ";"
 echo "}"
-echo
+echo "/////////////////////////////////////////////////////"
 echo
 echo "//Yes this is wrong, but its as far as I could get"
 
