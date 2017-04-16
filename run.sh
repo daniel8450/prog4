59c4cffb1f894a743609eedc66206d6f44dd243d
Daniel LaBorde <dlabo14@lsu.edu>
2017-04-16 17:21:58 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo
-echo "///BABELFISH///
+echo "///BABELFISH///"
 echo "I partially got the Java translation working, but no other languages are supported at this time.///"
 ./babelfish.class
 echo
