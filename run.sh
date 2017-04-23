4bfaeb24a21b0456ec8aaf36462d6032ce5583d4
Daniel LaBorde <dlabo14@lsu.edu>
2017-04-23 02:50:49 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,19 @@
 
 echo
 echo "///BABELFISH///"
-echo "I partially got the Java translation working, but no other languages are supported at this time.///"
+echo "Translations for Java, C, and Python///"
+echo "//// JAVA ////"
 java babelfish
+1
+echo "////////////"
+echo "//// C ////"
+java babelfish
+2
+echo "////////////"
+echo "//// PYTHON ////"
+java babelfish
+3
+echo "////////////"
+
 
 
